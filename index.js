3cee499d7a239fff3dd97c82a739dcd3eb0a28f9
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-06 12:37:39 -0500

    creando ruta de prueba

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.get('/',(request,response)=>{
+  response.json({error: "holo"})
+})
+
 const PORT = process.env.PORT
 console.log(PORT)
 const server = app.listen(PORT, () => {
