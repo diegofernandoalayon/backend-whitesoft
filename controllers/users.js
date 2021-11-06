371d524d70dab206cf75b15f246cbaf046f884a2
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-06 14:13:58 -0500

    creando ruta para guardar usuario y para adquirir usuarios

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,29 @@
 const userRouter = require('express').Router()
+const User = require('../models/userModel')
 
 userRouter.get('/', (request,response)=>{
+  User.find({})
+    .then((res)=>{
+      console.log(res)
+    })
   response.json({hola:'mundo'})
+})
+
+userRouter.post('/', (request, response) => {
+  console.log('hola')
+  console.log({request})
+  const { body } = request
+  console.log(body)
+  const { nombre, pais } = body
+
+  const user = new User({
+    nombre,
+    pais
+  })
+  user.save()
+    .then((savedUser)=>{
+      response.status(200).json(savedUser)
+    })
 })
 
 module.exports = userRouter
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 require('./mongo.js')
 const express = require('express')
+const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/',(request,response)=>{
   response.send('<h1>Hola</h1>')
